ea053df5ccab6810dbaed729568f14cc44dfbad9
xjxliuxing <xjxliuxing@163.com>
2023-07-09 23:03:14 +0800

    update code

--- app/src/main/java/com/android/convert/MainActivity.kt
+++ app/src/main/java/com/android/convert/MainActivity.kt
@@ -2,11 +2,14 @@ package com.android.convert
 
 import android.os.Bundle
 import android.os.Message
+import android.text.TextUtils
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.android.apphelper2.utils.HandlerUtil
 import com.android.apphelper2.utils.SocketUtil
+import com.android.apphelper2.utils.ToastUtil
+import com.android.apphelper2.utils.zmq.ZmqUtil6
 import com.android.convert.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
@@ -21,6 +24,9 @@ class MainActivity : AppCompatActivity() {
     }
     private val mHandler: HandlerUtil by lazy {
         return@lazy HandlerUtil()
+    }
+    private val mZmqUtil: ZmqUtil6.Result by lazy {
+        return@lazy ZmqUtil6.Result()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,11 +48,17 @@ class MainActivity : AppCompatActivity() {
                     val split = obj.split("|")
                     mBinding.tvSend.text = split[0]
                     mBinding.tvResult.text = split[1]
+                } else if (msg.what == 200) {
+                    val obj = msg.obj as String
+                    val split = obj.split("|")
+                    mBinding.tvZmqSend.text = split[0]
+                    mBinding.tvZmqResult.text = split[1]
                 }
             }
         })
 
         lifecycleScope.launch {
+            // 接收sock数据
             mSocketUtil.setServiceCallBackListener(object : SocketUtil.SocketService.ServerCallBackListener {
                 override fun callBack(send: String, result: String) {
                     val message = mHandler.getMessage()
@@ -55,16 +67,29 @@ class MainActivity : AppCompatActivity() {
                     mHandler.send(message)
                 }
             })
+
+            // 接收zmq数据
+            mZmqUtil.setResultCallBackListener(object : ZmqUtil6.Result.ResultCallBackListener {
+                override fun onCall(send: String, result: String) {
+                    val message = mHandler.getMessage()
+                    message.what = 200
+                    message.obj = "$send|$result"
+                    mHandler.send(message)
+                }
+            })
         }
 
+        // 初始化socket
         mBinding.btnInitSocket.setOnClickListener {
             mSocketUtil.initSocketService()
         }
 
+        // 停止socket
         mBinding.btnClose.setOnClickListener {
             mSocketUtil.stop()
         }
 
+        // 发送 socket 数据
         mBinding.btnSend.setOnClickListener {
             lifecycleScope.launch(Dispatchers.IO) {
                 repeat(Int.MAX_VALUE) {
@@ -75,6 +100,17 @@ class MainActivity : AppCompatActivity() {
                     delay(200)
                 }
             }
+        }
+
+        // 初始化zmq
+        mBinding.btnReceiver.setOnClickListener {
+            val ip = mBinding.tvIp.text.toString()
+            if (TextUtils.isEmpty(ip)) {
+                ToastUtil.show("Ip is empty !")
+                return@setOnClickListener
+            }
+            val tcp = "tcp://" + ip + ":" + ZmqUtil6.port
+            mZmqUtil.initResultZmq(tcp)
         }
     }
 }
